a1d50f28df22bff14604913a75942b28f96d4bdb
Chris Zuber <shgysk8zer0@gmail.com>
2025-12-04 16:04:54 -0800

    Update Node version, dependencies, and CI workflows
    Bump Node version to 24.10.0 in .nvmrc. Update multiple dependencies in package.json and package-lock.json. Improve GitHub Actions workflows for Node.js CI and npm publishing, including caching and setup steps. Switch ESLint config to use node.js rules. Update changelog for v1.1.28.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
 import { ignoreFile } from '@shgysk8zer0/eslint-config/ignoreFile.js';
-import node from '@shgysk8zer0/eslint-config/browser.js';
+import node from '@shgysk8zer0/eslint-config/node.js';
 
 export default [ignoreFile, node({ files: ['**/*.js'] })];
